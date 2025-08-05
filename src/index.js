5bd7c06100c10a025856b3cf913e62f9eb58105b
Katmitsu <ikaterynh@gmail.com>
2025-08-06 00:34:20 +0100

    added js funtions for timezone

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-//alert("hello world");
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss:SS [(<small>]A[</small>]"
+  );
+}, 1000);
+
+let lisbonElement = document.querySelector("#lisbon");
+let lisbonDateElement = lisbonElement.querySelector(".date");
+let lisbonTimeElement = lisbonElement.querySelector(".time");
+let lisbonTime = moment().tz("Europe/Lisbon");
+
+lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+lisbonTimeElement.innerHTML = lisbonTime.format(
+  "h:mm:ss:SS [(<small>]A[</small>]"
+);
