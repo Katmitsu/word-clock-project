8ebe8ac6670ccc80e3b99a69d3ddd48175a655c6
Katmitsu <ikaterynh@gmail.com>
2025-08-07 19:34:17 +0100

    Added the functionality for the select
    Everytime the user changes the value (city timezone) of the select it calls the function called updateCity: which is going to receive the timezone and then create a new city name based on the timezone so it's going to replace the underscore and the and use space split the name into two to second value then to New York only. then it is calculating the city timezone of the city with moment.tz from the current local time that we got from the dropdown bar. And then I am selecting the cities which are to be replaced with the whole HTML which includes the city name, the date and the current time and also the AM/PM.

--- src/index.js
+++ src/index.js
@@ -2,36 +2,50 @@
 
 setInterval(function () {
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss:SS [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss:SS [<small>]A[</small>]"
+    );
+  }
 }, 1000);
 
 //Lisbon Clock
 setInterval(function () {
   let lisbonElement = document.querySelector("#lisbon");
-  let lisbonDateElement = lisbonElement.querySelector(".date");
-  let lisbonTimeElement = lisbonElement.querySelector(".time");
-  let lisbonTime = moment().tz("Europe/Lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
 
-  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
-  lisbonTimeElement.innerHTML = lisbonTime.format(
-    "h:mm:ss:SS [<small>]A[</small>]"
-  );
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+    lisbonTimeElement.innerHTML = lisbonTime.format(
+      "h:mm:ss:SS [<small>]A[</small>]"
+    );
+  }
 }, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   console.log(cityTimeZone);
   let cityTime = moment().tz(cityTimeZone);
   console.log(cityTime.format("MMMM Do YYYY"));
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = "Hello";
+  citiesElement.innerHTML = `
+  <div class="city">  
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+       <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  `;
 }
 
 let citySelectElement = document.querySelector("#city");
